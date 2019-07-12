fade719e531803504f0aa0407f1b008b9c71d63f
hayleeb13 <hayleeb13@gmail.com>
2019-07-11 19:17:33 -0700

    Create react app

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer class="footer"><div class="bottom">Clicky Game!</div></footer>
+  );
+}
+
+export default Footer;
