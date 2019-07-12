b392bbb3bc5602681008a60c75f8636d09b50b8f
hayleeb13 <hayleeb13@gmail.com>
2019-07-11 19:16:57 -0700

    Updates

---
+++ precache-manifest.38b7573ffa7a5d340fa87af032bb3f69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80dd3568a49912c347e90f9c5e6f4195",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "d1f5e5be19005e9a3eea",
+    "url": "/clicky-game/static/css/main.5a04b4af.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "d1f5e5be19005e9a3eea",
+    "url": "/clicky-game/static/js/main.1d45b0c3.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
